2c12ba2aabce38a08559d484e81e1b8f02d8c980
Jordan Stoddard <jjsincorporated@gmail.com>
2019-07-15 15:55:46 -0700

    initialize component

---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Title() {
+    return (
+        <div className="title_container">
+            <h1>Conway's Game of Life</h1>
+        </div>
+    )
+}
