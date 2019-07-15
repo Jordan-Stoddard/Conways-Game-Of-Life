d6ff0f89476ad209a5e0287c8314ccee94559b90
Jordan Stoddard <jjsincorporated@gmail.com>
2019-07-15 15:55:09 -0700

    initialize component

---
+++ src/components/DefaultGridsContainer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function DefaultGridsContainer() {
+    return (
+        <div className="default_grids_container">
+            <button>Default Grid 1</button>
+            <button>Default Grid 2</button>
+            <button>Default Grid 3</button>
+            <button>Default Grid 4</button>
+        </div>
+    )
+}
