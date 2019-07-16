948d30285eab21362489c5282f36a982f21f7bc2
Jordan Stoddard <jjsincorporated@gmail.com>
2019-07-16 14:01:03 -0700

    complete manual automata

--- src/components/DefaultGridsContainer.js
+++ src/components/DefaultGridsContainer.js
@@ -1,12 +1,20 @@
-import React from 'react'
+import React from "react";
 
-export default function DefaultGridsContainer() {
-    return (
-        <div className="default_grids_container">
-            <button>Default Grid 1</button>
-            <button>Default Grid 2</button>
-            <button>Default Grid 3</button>
-            <button>Default Grid 4</button>
-        </div>
-    )
+export default function DefaultGridsContainer({ setDefaultGrid }) {
+  return (
+    <div className="default_grids_container">
+      <button value="Default Grid 1" onClick={setDefaultGrid}>
+        Default Grid 1
+      </button>
+      <button value="Default Grid 2" onClick={setDefaultGrid}>
+        Default Grid 2
+      </button>
+      <button value="Default Grid 3" onClick={setDefaultGrid}>
+        Default Grid 3
+      </button>
+      <button value="Default Grid 4" onClick={setDefaultGrid}>
+        Default Grid 4
+      </button>
+    </div>
+  );
 }
