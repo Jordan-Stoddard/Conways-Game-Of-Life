25264dc08faf60e6003196aa96717973bfee076b
Jordan Stoddard <jjsincorporated@gmail.com>
2019-07-17 15:26:02 -0700

    refactor RulesContainer component

--- src/components/RulesContainer.js
+++ src/components/RulesContainer.js
@@ -4,21 +4,40 @@ export default function RulesContainer() {
   return (
     <div className="rules_container">
       <h1>Rules of the Game:</h1>
+      <p>
+        The universe of the Game of Life is an infinite, two-dimensional
+        orthogonal grid of square cells, each of which is in one of two possible
+        states, alive or dead, (or populated and unpopulated, respectively).
+        Every cell interacts with its eight neighbours, which are the cells that
+        are horizontally, vertically, or diagonally adjacent. At each step in
+        time, the following transitions occur:
+      </p>
       <ul>
         <li>
-          If a cell is <span>alive</span> and it has exactly 2 or 3 live
-          neighbors, it stays alive.
+          Any live cell with fewer than two live neighbours dies, as if by
+          underpopulation.
         </li>
         <li>
-          {" "}
-          If a cell is <span>alive</span> and it has less than 2 or 4+ live
-          neighbors, it dies.
+          Any live cell with two or three live neighbours lives on to the next
+          generation.
         </li>
         <li>
-          If a cell is <span>dead</span> and it has exactly 3 live
-          neighbors, it <span>comes to life.</span>
+          Any live cell with more than three live neighbours dies, as if by
+          overpopulation.
+        </li>
+        <li>
+          Any dead cell with three live neighbours becomes a live cell, as if by
+          reproduction.
         </li>
       </ul>
+      <p>
+        The initial pattern constitutes the seed of the system. The first
+        generation is created by applying the above rules simultaneously to
+        every cell in the seed; births and deaths occur simultaneously, and the
+        discrete moment at which this happens is sometimes called a tick. Each
+        generation is a pure function of the preceding one. The rules continue
+        to be applied repeatedly to create further generations.
+      </p>
     </div>
   );
 }
