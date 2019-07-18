c769bbd4433afc7d8f168a22e314099639eb2c38
Jordan Stoddard <jjsincorporated@gmail.com>
2019-07-18 16:33:08 -0700

    Test performance

--- src/components/Title.js
+++ src/components/Title.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export default function Title() {
+    console.log("re-renders");
     return (
         <div className="title_container">
             <h1>Conway's Game of Life</h1>
